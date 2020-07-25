98b24c8d4cc5c25bd196fa2034a0ada2147fc92b
Niroshan Sugirtharatnam <codeniro@gmail.com>
2020-07-24 21:40:05 -0400

    minimize spacing for the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,13 +5,14 @@ import { Box } from "@material-ui/core";
 
 const Footer = () => {
   return (
-    <Box mt={8}>
+    <Box mt={6}>
       <Typography variant="body2" color="textSecondary" align="center">
         Copyright © {new Date().getFullYear() + " "}
         <Link color="inherit" href="https://www.nirosugir.com">
           Niroshan Sugirtharatnam
         </Link>
-        . All Rights Reserved.
+        .<br />
+        All Rights Reserved.
       </Typography>
     </Box>
   );
