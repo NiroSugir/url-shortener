b22c9b736937fabe8765ee2f37351c89b2cbe9a9
Niroshan Sugirtharatnam <codeniro@gmail.com>
2020-07-28 02:56:29 -0400

    add placeholder routes to backend

--- api/app/src/environment.js
+++ api/app/src/environment.js
@@ -1,6 +1,9 @@
 if (typeof process.env.PORT !== "string")
   throw new Error("Missing environment variable PORT");
 
+if (typeof process.env.HOSTNAME !== "string")
+  throw new Error("Missing environment variable HOSTNAME");
+
 if (typeof process.env.REDIS_HOST !== "string")
   throw new Error("Missing environment variable REDIS_HOST");
 
@@ -10,6 +13,7 @@ if (typeof process.env.REDIS_PORT !== "string")
 module.exports = {
   NODE_ENV:
     process.env.NODE_ENV === "production" ? "production" : "development",
+  HOSTNAME: process.env.HOSTNAME,
   PORT: process.env.PORT,
   REDIS_HOST: process.env.REDIS_HOST,
   REDIS_PORT: process.env.REDIS_PORT,
--- api/app/src/index.js
+++ api/app/src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const redis = require("redis");
 const morgan = require("morgan");
-const { NODE_ENV, REDIS_HOST, REDIS_PORT, PORT } = require("./environment");
+const {
+  NODE_ENV,
+  REDIS_HOST,
+  REDIS_PORT,
+  HOSTNAME,
+  PORT,
+} = require("./environment");
 
 const client = redis.createClient({ host: REDIS_HOST, port: REDIS_PORT });
 
@@ -10,7 +16,35 @@ const app = express();
 app.use(morgan(NODE_ENV === "production" ? "tiny" : "dev"));
 
 app.get("/", (_, res) => {
-  res.send("hi");
+  res.send({
+    message: `Redirection service. Go to ${HOSTNAME} to use the client. Copyright 2020 Niroshan Sugirtharatnam.`,
+  });
+});
+
+app.post("/create", (req, res) => {
+  const { slug, url } = req.body;
+
+  // TODO: validate
+
+  // TODO: create a slug if one wasn't provided
+
+  // TODO: add to db
+
+  res.sendStatus(200);
+});
+
+app.get("/:slug", (req, res) => {
+  const { slug } = req.params;
+
+  // TODO: validate
+
+  // TODO: retrieve slug from database
+
+  // TODO: add metric
+
+  // redirect to url found for slug
+  // TODO: replace placeholder url with the one found in the database
+  res.redirect(307, HOSTNAME);
 });
 
 app.use((err, req, res, next) => {
